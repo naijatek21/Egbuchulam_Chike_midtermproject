21eca899295f506128838d29bf40a5b7010403d5
naijatek21 <egbuchulamchike@gmail.com>
2025-10-15 16:46:05 -0400

    rule logic added

--- associationrules.py
+++ associationrules.py
@@ -33,15 +33,34 @@ def assocRules(dataTable,support_Val, confidence_Val):
         kSets = {}
         itemSets = itemLists(items,k)
         for s in itemSets:
-            count =0
-            for row in dataTable:
-                if s.issubset(row):
-                    count+=1
-            if count/len(dataTable) >= support_Val:
-                kSets.add(s)
+                count =0
+                for row in dataTable:
+                    if s.issubset(row):
+                        count+=1
+                if count/len(dataTable) >= support_Val:
+                    kSets.add(s)
         if len(kSets) == 0:
             supported = False
         current_set.union(kSets)
         k+=1
+
+    rule_preview= {i:[] for i in items}
+
+    for i in items:
+        for cSet in current_set:
+            if item in cSet
+                rule_preview[item].append(cSet - {item})
+
+    rule_tracker = {s : 0 from s in itemLists(items,2)}
+
+    for {a,b} in rule_tracker.keys():
+        for s in rule_preview[a]:
+            if b in s:
+                rule_tracker[(a,b)] +=1
+
+    for (a,b), count in rule_tracker.items():
+        confidence = count / len(rule_preview[a])
+        if confidence >= confidence_Val:
+            rules.append((a,b,confidence))
 
 
