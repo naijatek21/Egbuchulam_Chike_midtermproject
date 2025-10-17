194478faf24f8072ac6686451f90a3931f103915
Chike Egbuchulam <egbuchulamchike@gmail.com>
2025-10-17 19:08:46 -0400

    bare bones drawn out

---
+++ bfassociationrules.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import numpy as np
+import mlxtend as ml
+
+
+#used to create the item lists of k items
+def itemLists (items, k):
+
+    ilists = []
+
+
+    def backtrack(start, current):
+        if len(current) == k:
+            ilists.append(current[:])
+            return
+        for i in range(start, len(items)):
+            current.append(items[i])
+            backtrack(i + 1, current)
+            current.pop()
+
+    backtrack(0, [])
+    return ilists
+
+
+
+def assocRules(dataTable,support_Val, confidence_Val):
+    rules = []
+    supported = True
+    k=1
+    items =[]   #fill 
+    current_set = {}
+    while supported:
+        kSets = {}
+        itemSets = itemLists(items,k)
+        for s in itemSets:
+                count =0
+                for row in dataTable:
+                    if s.issubset(row):
+                        count+=1
+                if count/len(dataTable) >= support_Val:
+                    kSets.add(s)
+        if len(kSets) == 0:
+            supported = False
+        current_set.union(kSets)
+        k+=1
+
+    rule_preview= {i:[] for i in items}
+
+    for i in items:
+        for cSet in current_set:
+            if i in cSet:
+                rule_preview[i].append(cSet - {i})
+
+    rule_tracker = {s : 0 for s in itemLists(items,2)}
+
+    for {a,b} in rule_tracker.keys():
+        for s in rule_preview[a]:
+            if b in s:
+                rule_tracker[(a,b)] +=1
+
+    for (a,b), count in rule_tracker.items():
+        confidence = count / len(rule_preview[a])
+        if confidence >= confidence_Val:
+            rules.append((a,b,confidence))
+    return rules
+
+
--- midterm.py
+++ midterm.py
@@ -1,24 +1,95 @@
+import pandas as pd
+import numpy as np
+import mlxtend as ml
+from bfassociationrules import assocRules
+
+
 def main_menu():
-    while True:
-        print("\n=== Main Menu ===")
-        print("1. Add item")
-        print("2. View items")
-        print("3. Delete item")
-        print("4. Exit")
-
-        choice = input("Enter your choice (1-4): ")
-
-
-stores = ["Alice's Bakery", "Bob's Breakfasts", "Charlie's Cafe", "Desayuno de David","Eddie's Eatery"]
-
-
-def view_items():
-    if not stores:
-        print("No items found.")
-    else:
-        print("\nItems:")
-        for i, store in enumerate(stores, 1):
+    storeChoice = ""
+    dbfile = ""
+    stores = ["Alice's Bakery", "Bob's Breakfast", "Charlie's Cafe", "Desayuno de David","Eddie's Eatery"]
+    supportValue,confidenceValue=0
+    print("\n==================Welcome to the Midterm Baking District==============")
+    print("Please Select a Store")
+    for i, store in enumerate(stores, 1):
             print(f"{i}. {store}")
+    choice = input("Enter your choice (1-5): ")
+    
+    while storeChoice=="":
+        if choice == "1":
+            storeChoice="Alice's Bakery"
+            dbfile='alicebakery001.csv'
+        elif choice == "2":
+            storeChoice="Bob's Breakfast"
+            dbfile='bobsbreakfast001.csv'
+        elif choice == "3":
+            storeChoice="Charlie's Cafe"
+            dbfile='charliescafe001.csv'
+        elif choice == "4":
+            storeChoice="Desayuno de David"
+            dbfile='desayunodedavid001.csv'
+        elif choice == "5":
+            storeChoice="Eddie's Eatery"
+            dbfile='eddieseats001.csv'
+            break
+        else:
+            print("Invalid choice. Please choose a number 1-5.")
+
+    print("Welcome to "+storeChoice+"!")
+
+    # input validation
+    while True:
+        sV=("Please enter the minimum support %% you want (0-100): ")
+        try:
+            supportValue = (int(sV))/100
+        except ValueError:
+            print("Please input a numeric value")
+            continue
+        if 0.0<=supportValue<=1.00:
+            break
+        else:
+            print("Invalid selection")
+
+
+    while True:
+        cV=input("Please enter the minimum support %% you want (0-100): ")
+        try:
+            confidenceValue = (int(cV))/100
+        except ValueError:
+            print("Please input a numeric value")
+            continue
+        if 0.0<=confidenceValue<=1.0:
+            break
+        else:
+            print("Invalid selection")
+
+    transactions= pd.read_csv(dbfile,usecols=["ItemsPurchased"])
+
+    bfrules=assocRules(dbfile,supportValue,confidenceValue)
+    
+'''
+ te = TransactionEncoder()
+ te_array = te.fit(transactions).transform(transactions)
+ df_encoded = pd.DataFrame(te_array, columns=te.columns_)
+ frequent_items = apriori(df_encoded, min_support=s upportValue, use_colnames=True)
+ aRules = association_rules(frequent_items, metric="confidence", min_threshold=confidenceValue)
+ rules = rules[rules['antecedents'].apply(lambda x: len(x) >= 1) & rules['consequents'].apply(lambda x: len(x) >= 1)]
+
+
+
+
+ treeRes=fpgrowth(df, min_support=supportValue)
+ fpRules = association_rules(frequent_itemsets, metric='confidence', min_threshold=confidenceValue)
+
+
+'''
+ 
+
+
+    
+
+
+
 
 
 
